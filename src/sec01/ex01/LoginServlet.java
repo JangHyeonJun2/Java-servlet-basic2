5f3796283f6f3dd04adbeb1aa5ef5ec1bcaab3b0
JangHyeonJun2 <epfvkdlxj@naver.com>
2019-09-12 22:10:19 +0900

    <hidden>태그를 이용한 세션 트래킹 실습

---
+++ src/sec01/ex01/LoginServlet.java
@@ -0,0 +1,47 @@
+package sec01.ex01;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init 메서드 호출");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        String user_id = req.getParameter("user_id");
+        String user_pw = req.getParameter("user_pw");
+        String user_address = req.getParameter("user_address");
+        String user_email = req.getParameter("user_email");
+        String user_hp = req.getParameter("user_hp");
+
+        String data = "안녕하세요!<br> 로그인 하셨습니다.<br><br>";
+        data+="<html><body>";
+        data+="아이디: "+user_id;
+        data+="<br>";
+        data+="패스워드: "+user_pw;
+        data+="<br>";
+        data+="주소: "+user_address;
+        data+="<br>";
+        data+="이메일: "+user_email;
+        data+="<br>";
+        data+="전화번호: "+user_hp;
+        data+="</body></html>";
+        out.print(data);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("destroy 메서드 호출");
+    }
+}
