c7a46ed1a85cd21952a17dcdac8742c71cf904d2
JangHyeonJun2 <epfvkdlxj@naver.com>
2020-02-25 22:32:12 +0900

    encodeURL()메서드를 이용한 세션 사용 실습

--- src/sec04/ex01/SessionTest5.java
+++ src/sec04/ex01/SessionTest5.java
@@ -1,4 +1,4 @@
-package sec03.ex04;
+package sec04.ex01;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +8,9 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-/*
- * 세션 이용하기
- */
-//@WebServlet("/login")
-public class SessionTest4 extends HttpServlet {
+
+@WebServlet("/login")
+public class SessionTest5 extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doHandle(req,resp);
@@ -22,22 +20,21 @@ public class SessionTest4 extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         doHandle(req,resp);
     }
-    //직접 만든 메서드
-    public void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException,IOException{
+
+    public void doHandle(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
         req.setCharacterEncoding("utf-8");
         resp.setContentType("text/html;charset=utf-8");
         PrintWriter out = resp.getWriter();
         HttpSession session = req.getSession();
-
         String user_id = req.getParameter("user_id");
         String user_pwd = req.getParameter("user_pwd");
-
         if(session.isNew()){
             if(user_id != null){
                 session.setAttribute("user_id",user_id);
-                out.println("<a href='login'>로그인 상태 확인</a>");
+                String url=resp.encodeURL("login");//변수 url에 encodURL()을 이용해 응답 시 미리 jsessionID를 저장합니다.
+                out.println("<a href="+url+">로그인 상태 확인</a>"); //로그인 상태 확인 클릭시 jsessionId를 서블릿으로 다시 전송한다.
             }else {
-                out.println("<a href='login2.html'>다시 로그인하세요!!</a>");
+                out.println("<a href='login2.html'>다시 로그인하시오!!</a>");
                 session.invalidate();
             }
         }else {
